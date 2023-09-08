5a2669fdf5e8508bb30dcd918f5cdf1f4d2472a4
rachanap30 <138125282+rachanap30@users.noreply.github.com>
2023-09-08 10:08:00 +0200

    Update movie_recommendation_r.py

--- movie_recommendation_r.py
+++ movie_recommendation_r.py
@@ -206,8 +206,6 @@ n = 10  # Number of recommendations
 recommendations = get_movie_recommendations(movies_ratings, movie_id, n)
 recommendations
 
-pip install scikit-surprise
-
 from surprise import Dataset, Reader
 from surprise.model_selection import train_test_split
 from surprise import NormalPredictor, KNNBasic, KNNWithMeans, SVD
